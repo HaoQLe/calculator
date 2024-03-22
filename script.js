0844bbb61705785d1ee14c7e0ddd80d19d60ed95
Hao Le <71927521+HaoQLe@users.noreply.github.com>
2024-03-21 20:12:59 -0700

    Finish operate function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let opOne; // Op for operand
+let opTwo;
+let operator;
+
 function add(x, y) {
     return x + y;
 }
@@ -12,4 +16,21 @@ function multiply(x, y) {
 
 function divide(x, y) {
     return x / y;
+}
+
+function operate(opOne, opTwo, operator) {
+    switch (operator) {
+        case ('+'):
+            return add(opOne, opTwo);
+            break;
+        case ('-'):
+            return subtract(opOne, opTwo);
+            break;
+        case ('*'):
+            return multiply(opOne, opTwo);
+            break;
+        case ('/'):
+            return divide(opOne, opTwo);
+            break;
+    } 
 }
